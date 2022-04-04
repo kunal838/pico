21bd73f04429b8af27305a58a8f651c43a21071a
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-04-04 11:37:06 +0530

    Added React Draw and fixed the layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import Image from "next/image";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { MdAddPhotoAlternate } from "react-icons/md";
+import { MdClose } from "react-icons/md";
+import { MdArrowBack } from "react-icons/md";
+import { MdOutlineCode } from "react-icons/md";
+import { MdDraw } from "react-icons/md";
+import Draw from "./components/Draw";
 
 export default function Home() {
   const [imageSrc, setImageSrc] = useState();
   const [uploadData, setUploadData] = useState();
-  const [toggle,settoggle] = useState(false);
-  const CodeEditor = dynamic(import("./components/CodeEditor"), {
+  const [toggle, settoggle] = useState(true);
+  const [Model, setShowModel] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const filepickRef = useRef();
+  const DynamicCodeEditor = dynamic(() => import("./components/CodeEditor"), {
+    ssr: false,
+  });
+  const DynamicOutput = dynamic(() => import("./components/Output"), {
     ssr: false,
   });
 
@@ -36,77 +49,145 @@ export default function Home() {
   }
 
   return (
-    <main classNameName="">
-        <img  src="./2.png" className="blur z-0 absolute bottom-0 left-0 md:h-screen md:w-screen opacity-60 h-3/4 md:w-screen " alt="blur"/> 
+    <main className='h-screen w-screen'>
+      <img
+        src='./2.png'
+        className='blur -z-10 absolute bottom-0 left-0 md:h-screen md:w-screen opacity-60 h-3/4'
+        alt='blur'
+      />
 
-      <nav className="  bg-blur bg-white border-gray-200 px-2 sm:px-4 py-2.5 bg-transparent  dark:bg-white">
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <a  className="flex items-center">
-           
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-black">
+      {/* <nav className='  bg-blur bg-white border-gray-200 px-2 sm:px-4 py-2.5 bg-transparent  dark:bg-white'>
+        <div className='container flex flex-wrap justify-between items-center mx-auto'>
+          <a className='flex items-center'>
+            <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-black'>
               PICO-BITE
             </span>
           </a>
-          <div className="flex md:order-2">
+          <div className='flex md:order-2'>
             <button
-            
-              type="button"
-              className="text-white uppercase bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-gray-900 dark:hover:bg-gray-700 shadow-xl dark:focus:ring-blue-800"
+              type='button'
+              className='text-white uppercase bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-gray-900 dark:hover:bg-gray-700 shadow-xl dark:focus:ring-blue-800'
             >
               Give me a Star on github
             </button>
-          
           </div>
         </div>
-      </nav>
- 
+      </nav> */}
+
       {/* tabbar */}
-      <div class=" backdrop-blur-3xl mx-40 sm:px-4 py-2.5 bg-transparent md:ml-32  max-h-3xl item-center justify-center border-b border-gray-100 m-3 dark:border-gray-100">
-    <ul class="flex  items-center  flex-wrap -mb-px text-sm font-bold text-center sticky text-gray-900 dark:text-gray-900">
-        <li onClick={()=>settoggle(true)} class="mr-2">
-            <a href="#" class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-900 group">
-                <svg class="mr-2 w-5 h-5 text-gray-900 group-hover:text-gray-900 dark:text-gray-900 dark:group-hover:text-gray-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>Profile
+      <div className=' backdrop-blur-3xl  bg-transparent max-h-3xl item-center justify-between border-b border-gray-100 dark:border-gray-100 flex flex-row px-4'>
+        <ul className='flex  items-center  flex-wrap -mb-px text-sm font-bold text-center sticky text-gray-900 dark:text-gray-900'>
+          <div className='mr-4 hover:scale-[1.2] ease-linear duration-100 cursor-pointer'>
+            <MdArrowBack size={24} />
+          </div>
+          <li onClick={() => settoggle(true)} className='mr-2'>
+            <a
+              href='#'
+              className='focus:text-black text-gray-500 inline-flex p-4 rounded-t-lg border-2 ease-linear duration-100 focus:border-b-black border-transparent hover:border-b-gray-200'
+            >
+              <MdOutlineCode size={24} />
+              <div className='mx-2'>Code</div>
             </a>
-        </li>
-        <li onClick={()=>settoggle(false)} class="mr-2">
-            <a href="#" class="inline-flex p-4 text-blue-600 rounded-t-lg border-b-2 border-blue-600  dark:border-gray-500 " aria-current="page">
-                <svg class="mr-2 w-5 h-5 text-gray-700 dark:text-gray-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>Dashboard
+          </li>
+          <li onClick={() => settoggle(false)} className='mr-2'>
+            <a
+              href='#'
+              className='focus:text-black text-gray-500 inline-flex p-4 items-center ease-linear duration-100 focus:border-b-black hover:border-b-gray-200 border-2 border-transparent'
+            >
+              <MdDraw size={24} />
+              <div className='mx-2'>Whiteboard</div>
             </a>
-        </li>
-       
-        
-    </ul>
-</div>
+          </li>
+        </ul>
+        <a className='hidden md:inline-block text-white uppercase bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-gray-900 dark:hover:bg-gray-700 shadow-xl ease-linear duration-100 active:scale-[0.9] self-center cursor-pointer'>
+          Give me a star ⭐
+        </a>
+      </div>
 
       {/* main section */}
-      {toggle?(
-        <form className=" z-50" method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
-        <p>
-          <input  className=" " type="file" name="file" />
-        </p>
+      {toggle ? (
+        <form
+          className='w-screen flex flex-col-reverse md:flex-row-reverse justify-between'
+          method='post'
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
+        >
+          <div className='w-full md:w-[30%] m-4 ml-2 '>
+            <DynamicOutput />
+            <div className='pr-2 pl-3 flex flex-row'>
+              <button
+                type='button'
+                className=' h-[50px] w-full mr-2 text-white uppercase bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm  text-center dark:bg-gray-900 dark:hover:bg-gray-700 shadow-xl ease-linear duration-100 active:scale-[0.9]'
+              >
+                run code
+              </button>
+              <button
+                type='button'
+                onClick={() =>{ setShowModel(!Model);}}
+                className='h-[50px] w-full ml-2 text-white uppercase bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm  text-center dark:bg-gray-900 dark:hover:bg-gray-700 shadow-xl ease-linear duration-100 active:scale-[0.9]'
+              >
+                Upload Image
+              </button>
+            </div>
+          </div>
 
-        <img src={imageSrc} />
+          {/*MODEL */}
+          {Model && !uploadData && (
+            <div className=' fixed backdrop-blur-md bg-[rgba(0,0,0,0.4)]  w-[100vw] h-[100vh]  z-50 top-0 flex justify-center items-center'>
+              <div className='w-[300px]  rounded-2xl bg-white shadow-xl flex flex-col justify-around items-center'>
+                {imageSrc ? (
+                  <img
+                    alt='uploaded image'
+                    src={imageSrc}
+                    height={150}
+                    width={250}
+                    className='rounded-2xl my-6'
+                  />
+                ) : (
+                  <button
+                    onClick={() => filepickRef.current.click()}
+                    className=' cursor-crosshair'
+                  >
+                    <input
+                      ref={filepickRef}
+                      hidden
+                      className=' '
+                      type='file'
+                      name='file'
+                    />
+                    <MdAddPhotoAlternate
+                      size={150}
+                      className=' text-gray-200 mt-10'
+                    />
+                  </button>
+                )}
+                <div
+                  className='h-10 w-10 bg-white border-2 absolute mb-64 ml-72 flex justify-center items-center rounded-full cursor-pointer hover:scale-[1.2] ease-linear duration-100'
+                  onClick={() => setShowModel(false)}
+                >
+                  <MdClose size={20} />
+                </div>
+                <button
+                  type='submit'
+                  onClick={()=>setLoader(!loader)}
+                  className='text-white uppercase bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-gray-900 dark:hover:bg-gray-700 shadow-xl ease-linear duration-100 active:scale-[0.9] my-4 mb-6 flex flex-row items-center'
+                >
+                {loader&&<div className="h-6 w-6 border-[3px] rounded-full bg-transparent border-blue-200 border-b-blue-700 animate-spin mr-4"></div>}
+                <div >
+                  Upload Image
+                </div>
+                </button>
+              </div>
+            </div>
+          )}
 
-        {imageSrc && !uploadData && (
-          <p>
-            <button className=" z-50">Upload Files</button>
-          </p>
-        )}
-
-        {/* {uploadData && (
-        
-        )} */}
-
-<CodeEditor uploadData={uploadData} />
-        
-
-
-      </form>
-      ):(
-        <CodeEditor />
+          <div className='w-full md:w-[70%]'>
+            <DynamicCodeEditor uploadData={uploadData} />
+          </div>
+        </form>
+      ) : (
+          <Draw/>
       )}
-      
     </main>
   );
 }
-
